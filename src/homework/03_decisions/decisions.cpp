1ae4ecdfdadcafbb57b58941b9e997054f239383
CBo73 <cody.bowen@g.austincc.edu>
2022-09-22 09:24:47 +0000

    Finished HW 3

--- src/homework/03_decisions/decisions.cpp
+++ src/homework/03_decisions/decisions.cpp
@@ -1,4 +1,64 @@
 //write include statement for decisions header
-
+#include "decisions.h"
 
 //Write code for function(s) code here
+std::string get_letter_grade_using_if(int grade)
+{
+    std::string letter_grade;
+
+    if (grade > 89 && grade < 101)
+    {
+        letter_grade = "A"; 
+    }
+    else if(grade > 79 && grade < 90)
+    {
+        letter_grade = "B";
+    }
+    else if(grade > 69 && grade < 80)
+    {
+        letter_grade = "C";
+    }
+    else if(grade > 59 && grade < 70)
+    {
+        letter_grade = "D";
+    }
+    else if (grade > 0 && grade <60)
+    {
+        letter_grade = "F";
+    }
+    else
+    {
+        letter_grade = "Invalid score";
+    }
+
+    return letter_grade;
+}
+
+std::string get_letter_grade_using_switch(int grade)
+{
+   std::string letter_grade;
+
+   switch(grade)
+   {
+    case 90 ... 100:
+        letter_grade = "A";
+        break;
+    case 80 ... 89:
+        letter_grade = "B";
+        break;
+    case 70 ... 79:
+        letter_grade = "C";
+        break;
+    case 60 ... 69:
+        letter_grade = "D";
+        break;
+    case 00 ... 59:
+        letter_grade = "F";
+        break;
+    default:
+        letter_grade = "Invalid Grade";
+        break;
+   }
+
+   return letter_grade; 
+}
--- src/homework/03_decisions/main.cpp
+++ src/homework/03_decisions/main.cpp
@@ -1,6 +1,38 @@
 //write include statements
+#include "decisions.h"
+#include<iostream>
+
+using std::cout;
+using std::cin;
+using std::string;
 
 int main() 
 {
+	int choice;
+	int grade;
+	string result;
+
+	cout<<"1. Letter Grade Using if"<<"\n"<<"2. Letter Grade Using switch"<<"\n"<<"3. Exit"<<"\n";
+	cin>>choice;
+
+	switch(choice)
+	{
+		case 1:
+			cout<<"IFELSEIF: Please enter how many percentage points earned Ex: 92"<<"\n";
+			cin>>grade;
+			result = get_letter_grade_using_if(grade);
+			cout<<result<<"\n";
+			break;
+		case 2:
+			cout<<"SWTICH: Please enter how many percentage points earned Ex: 92"<<"\n";
+			cin>>grade;
+			result = get_letter_grade_using_switch(grade);
+			cout<<result<<"\n";
+			break;
+		case 3:
+			cout<<"Have a good day!";
+			break;
+	}
+
 	return 0;
 }
