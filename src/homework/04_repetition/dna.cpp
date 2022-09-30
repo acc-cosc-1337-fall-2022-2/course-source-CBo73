c34191133a0051c90ce46bda62ec23ed2ad7365b
CBo73 <cody.bowen@g.austincc.edu>
2022-09-30 16:27:00 +0000

    HW 4 submission

--- src/homework/04_repetition/dna.cpp
+++ src/homework/04_repetition/dna.cpp
@@ -1,3 +1,37 @@
 //add include statements
+#include "dna.h"
 
 //add function(s) code here
+int factorial(int num)
+{
+   int sum = 1;
+
+   while (num > 0)
+   {
+      sum *= num;
+      num--;
+   }
+   return sum;
+   
+}
+
+int gcd(int num1,int num2)
+{
+   int temp;
+
+   while (num1 != num2)
+   {
+      if (num1 < num2)
+      {
+         temp = num1;
+         num1 = num2;
+         num2 = temp;
+      }
+
+      if (num1 > num2)
+      {
+         num1 = num1-num2;
+      }
+   }
+   return num1;
+}
--- src/homework/04_repetition/main.cpp
+++ src/homework/04_repetition/main.cpp
@@ -1,7 +1,10 @@
 //write include statements
+#include <iostream>
+#include "dna.h"
 
 //write using statements
-
+using std::cout;
+using std::cin;
 
 /*
 Write code that prompts user to enter 1 for Get GC Content, 
@@ -12,5 +15,45 @@ user enters a y or Y.
 */
 int main() 
 {
+	int choice;
+	int num;
+	int num1;
+	int num2;
+	int exprogram = 0;
+	
+	do
+	{
+		cout <<"\n1) for Factorial\n2) for Greatest Common Divisor\n3) for Exit\n";
+		
+		cin >> choice;
+
+		if (choice == 1)
+		{
+			cout<<"Please enter a positive integer"<<"\n";
+			cin>>num;
+			cout<<"The Factorial of your number is: "<<factorial(num)<<"\n";
+
+		}
+		if (choice == 2)
+		{
+			cout<<"Please enter the first integer for GCD"<<"\n";
+			cin>>num1;
+			cout<<"Please enter the second integer for GCD"<<"\n";
+			cin>>num2; 
+			cout<<"The GCD of these numbers is: "<<gcd(num1,num2)<<"\n";
+
+		}
+
+		else if (choice == 3)
+		{
+			cout<<"Are you sure you would like to quit? Press 0 to continue or 1 to exit"<<"\n";
+			cin>>exprogram;
+
+		}
+	} 
+	while (exprogram == 0);
+
+	cout<<"\nTake Care!\n";
+		
 	return 0;
 }
