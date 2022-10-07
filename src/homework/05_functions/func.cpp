816289e4c9d10f8fe82c6ead73a714e4438ed742
CBo73 <cody.bowen@g.austincc.edu>
2022-10-07 18:33:51 +0000

    HW_5_finalcode

--- src/homework/05_functions/func.cpp
+++ src/homework/05_functions/func.cpp
@@ -1,3 +1,68 @@
 //add include statements
+#include <string>
+#include "func.h"
+using std::string;
 
 //add function code here
+
+double get_gc_content(const string& dna)
+{
+    double counter = 0;
+    int i;
+    double division;
+
+    for(int i = 0; i < dna.length(); i++)
+    {
+        if (dna[i] == 'C' || dna[i] == 'c' || dna[i] == 'g' || dna[i] == 'G')
+        {
+            counter++;
+        }
+
+    }
+
+    division = counter / dna.length();
+
+return division;
+
+}
+
+string get_dna_complement(string dna)
+{
+    dna = reverse_string(dna);
+
+    for(int i = 0; i < dna.length(); i++)
+    {
+        if (dna[i] == 'c' || dna[i] == 'C')
+        {
+            dna[i] = 'G';
+        }
+        else if (dna[i] == 'g' || dna[i] == 'G')
+        {
+            dna[i] = 'C';
+        }
+        else if (dna[i] == 'a' || dna[i] == 'A')
+        {
+            dna[i] = 'T';
+        }
+        else if (dna[i] == 't' || dna[i] == 'T')
+        {
+            dna[i] = 'A';
+        }
+    }
+
+    return dna;
+}
+
+string reverse_string(string dna)
+{
+    string reverse_of_dna = dna;
+
+    for (int i= 0; i < dna.length(); i++)
+    {
+        if (dna[i] == 'a' || dna[i] == 'A' || dna[i] == 't' || dna[i] == 'T' || dna[i] == 'g' || dna[i] == 'G' || dna[i] == 'c' || dna[i] == 'C')
+        {
+            dna[i] = reverse_of_dna[(dna.length()-1) - i];
+        }
+    }
+    return dna;
+}
--- src/homework/05_functions/main.cpp
+++ src/homework/05_functions/main.cpp
@@ -1,3 +1,9 @@
+#include <iostream>
+#include "func.h"
+
+using std::cout;
+using std::cin;
+
 /*
 use a vector of int with values 8, 4, 20, 88, 66, 99
 
@@ -9,6 +15,46 @@ Program continues until user decides to exit.
 */
 int main() 
 {
+	int choice;
+	int exprogram = 0;
+	double gc_content;
+	string dna_complement;
+	string dna;
+
+	do
+	{
+		cout<<"\n1) for Get GC Content\n2) for Get DNA Complement\n3) for Exit\n";
+		cin>>choice;
+
+		if (choice == 1)
+		{
+			cout<<"\nPlease enter the DNA string\n";
+			cin>>dna;
+			gc_content = get_gc_content(dna);
+			cout<<"\nThe GC content is: "<<gc_content<<"\n";
+
+
+		}
+
+		else if (choice == 2)
+		{
+			cout<<"\nPlease enter the DNA string\n";
+			cin>>dna;
+			dna_complement = get_dna_complement(dna);
+			cout<<"\nThe DNA content is: "<<dna_complement<<"\n";
+
+		}
+
+		else
+		{
+			cout<<"\nAre you sure you would like to quit? Press 0 to continue or 1 to exit"<<"\n";
+			cin>>exprogram;
+		}
+
+	}
+	while(exprogram == 0);
+
+	cout<<"\nHave a Good Day!\n";
 
 	return 0;
 }
