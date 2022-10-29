e0584326793e53fea692ff44089b21a5ea151f2b
CBo73 <cody.bowen@g.austicc.edu>
2022-10-29 17:04:00 -0500

    Finished HW6 TicTacToe

--- src/homework/06_tic_tac_toe/main.cpp
+++ src/homework/06_tic_tac_toe/main.cpp
@@ -1,7 +1,48 @@
+#include <iostream>
+#include "tic_tac_toe.h"
 
-int main() 
+using std::cout;
+using std::cin;
+
+int main()
 {
-    //dfd
+    TicTacToe game;
 
+    string first_player;
+    int position;
+    int select = 0;
+
+    while (select == 0)
+    {
+        cout << "Player One: Choose X or O\n";
+        cin >> first_player;
+
+        if (first_player == "X" || first_player == "O")
+        {
+            game.start_game(first_player);
+            do
+            {
+                cout << "Player " << game.get_player() << " choose position 1-9\n";
+                game.display_board();
+                cin >> position;
+                game.mark_board(position);
+            }
+            while (!game.game_over());
+
+            game.display_board();
+            game.game_over();
+            cout << "Would you like to play again? Press 0 for continue or 1 to Exit\n";
+            cin >> select;
+        }
+        else
+        {
+            cout << "Invalid entry. Please try again\n";
+        }
+    }
+    while (select != 0)
+    {
+        cout<<"Thank you for playing!";
+        break;
+    }
 	return 0;
 }
--- src/homework/06_tic_tac_toe/tic_tac_toe.h
+++ src/homework/06_tic_tac_toe/tic_tac_toe.h
@@ -1,1 +1,33 @@
 //h
+#include <iostream>
+#include <string>
+#include <vector>
+#include<algorithm>
+
+#ifndef TicTacToe_H
+#define TicTacToe_H
+
+using std::string;
+using std::vector;
+using std::cout;
+using std::cin;
+
+class TicTacToe
+{
+
+public:
+    bool game_over();
+    void start_game(string first_player);
+    void mark_board(int position);
+    string get_player() const;
+    void display_board() const;
+
+private:
+    string player;
+    vector<string> pegs{" "," "," "," "," "," "," "," "," "};
+    void set_next_player();
+    bool check_board_full();
+    void clear_board();
+};
+
+#endif
